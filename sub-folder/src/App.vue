fc650f5780729e0edd48b2a4c0990ba1c9433a5e
Reinier Kaper <rp.kaper+github@gmail.com>
2021-11-03 19:59:47 -0400

    feat: :sparkles: Minimal repro for Volar issue

---
+++ sub-folder/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img v-if="needRefresh" alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useRegisterSW } from 'virtual:pwa-register/vue'
+
+export default defineComponent({
+  name: 'App',
+
+  setup () {
+    const { needRefresh } = useRegisterSW()
+
+    return {
+      needRefresh,
+    }
+  },
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
